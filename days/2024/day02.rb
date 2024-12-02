fd266923169eccce755718c791f48ae9c3b59748
aaaeide <andreasaaeide@gmail.com>
2024-12-02 13:46:27 +0100

    tidy day02

--- days/2024/day02.rb
+++ days/2024/day02.rb
@@ -4,41 +4,13 @@ def read_input(fname)
   File.readlines(fname, chomp: true).map { |ln| ln.split.map(&:to_i) }
 end
 
-def arr_except_idx(arr, idx)
-  arr.reject.with_index { |_, i| i == idx }
+def check(report)
+  inc_or_dec = -> { report == report.sort || report == report.sort.reverse }
+  small_diff = -> { report.each_cons(2).map { |x, y| (x - y).abs }.to_set - Set[1, 2, 3] == Set[] }
+
+  inc_or_dec.call && small_diff.call
 end
 
-def check_report(report)
-  sorted = report.sort
-  return false unless report == sorted || report == sorted.reverse
-
-  1.step(to: report.length - 1).each do |i|
-    diff = (report[i] - report[i - 1]).abs
-    return false unless [1, 2, 3].include?(diff)
-  end
-
-  true
-end
-
-def dampened_check_report(report)
-  return true if check_report(report)
-
-  0.step(to: report.length - 1).each do |i|
-    return true if check_report(arr_except_idx(report, i))
-  end
-
-  false
-end
-
-def part1
-  reports = read_input('data/2024/02/data.txt')
-  reports.count { |report| check_report(report) }
-end
-
-def part2
-  reports = read_input('data/2024/02/data.txt')
-  reports.count { |report| dampened_check_report(report) }
-end
-
-p part1
-p part2
+reports = read_input('data/2024/02/data.txt')
+puts('part1', reports.count { |rep| check(rep) })
+puts('part2', reports.count { |rep| rep.each_index.any? { |i| check(rep.reject.with_index { |_, j| j == i }) } })
